1f045215fdd64bee5e50fb22080d8e14eda1631e
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-11-23 14:04:04 +0530

    modify: middleware position

--- server/index.js
+++ server/index.js
@@ -35,15 +35,14 @@ csvRead.on("end", () => {
 	app.get("/books", (req, res) => {
 		res.json(csvData);
 	});
+});
 
-	// add middlewares
-	app.use(express.static(path.join(__dirname, "../client/build")));
-	app.use(express.static(path.join(__dirname, "../client/public")));
+// add middlewares
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-	// All other GET requests not handled before will return our React app
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../client/build", "/index.html"));
-	});
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../client/build", "/index.html"));
 });
 
 app.listen(PORT, () => {
